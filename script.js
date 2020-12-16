c836c89074038cd6be26f9aa5bce87d1d267d70f
Can Seymen <can.seymen@orgino.com.tr>
2020-12-16 21:37:06 +0300

    edited

---
+++ script.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-red", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-red";
+}
+
+
+
+/* Mail format kontrolü */
+
+
+function mailKontrol() {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var mail = document.getElementById("mail").value;
+    if (reg.test(mail) == false) {
+        alert('Hatalı Mail Formatı!');
+        return false;
+    }
+    else {
+        
+        return true;
+    }
+}
