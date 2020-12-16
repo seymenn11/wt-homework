bb70523b3ccae9ce0657ca413624207ff6f8cabf
Can Seymen <can.seymen@orgino.com.tr>
2020-12-17 00:43:22 +0300

    edited

--- script.js
+++ script.js
@@ -26,6 +26,15 @@ function showDivs(n) {
 }
 
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
 
 /* Mail format kontrolü */
 
